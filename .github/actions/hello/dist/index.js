b63828907c7eb48a7a7390d971ee2e3820c7b894
JC-Coder <josephchimezie2003@gmail.com>
2024-01-16 17:23:12 +0100

    update

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -30794,13 +30794,22 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 try {
+    core.debug('Debug message')
+    core.warning('Warning message')
+    core.error('Error message')
+
     const name = core.getInput("who-to-greet");
+    core.setSecret(name) // will mask data -> ****
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging github object') // enable expanding and collapsing data
     console.log(JSON.stringify(github, null, "\t"));
+    core.endGroup()
+
+    core.exportVariable('HELLO', 'hello')
 } catch (e) {
     core.setFailed(e.message);
 }
