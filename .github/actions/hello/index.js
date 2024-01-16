f91b1f2ab48b8dc555c1b73b8693958d8b6f4a1b
JC-Coder <josephchimezie2003@gmail.com>
2024-01-16 17:03:33 +0100

    add custom action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+    const name = core.getInput("who-to-greet");
+    console.log(`Hello ${name}`);
+
+    const time = new Date();
+    core.setOutput("time", time.toTimeString());
+
+    console.log(JSON.stringify(github, null, "\t"));
+} catch (e) {
+    core.setFailed(e.message);
+}
